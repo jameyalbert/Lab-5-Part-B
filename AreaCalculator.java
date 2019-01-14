8068b03102e08c67e1a4ed38f934c0fc93070fa6
Andrew Dunham <37754965+AndrewDunham@users.noreply.github.com>
2019-01-14 11:13:11 -0800

    Update AreaCalculator.java

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -12,70 +12,89 @@ public class AreaCalculator {
 	static Scanner scanner = new Scanner(System.in);//Static will not be interacting with objects.
 	public static void main(String[] args){//void will not return any values.
 
-		//Declaration of objects that will be used. 
+		//Declaration of variables that will be used. 
 		int objectToCalculate;
 		double base; 
 		double height;
 		double radius;
 		double edge;
 		double area;
-		String startAgain;
+		Boolean startAgain = true;
 
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~AREA CALCULATOR~~~~~~~~~~~~~~~~~~~~~");//Prints with a new line at the end hence the ln(line)
 		System.out.println("1. Area of a Rectangle/square\n2. Area of a Triangle\n3. Area of a Circle\n4. Area of a Cube\n5. Area of a Cone\n6. Area of Sphere");
-		System.out.print("\nEnter the number of the object you wish to calculate: ");//print prints without a new line
-		objectToCalculate = scanner.nextInt();
-		
-		if (objectToCalculate == 1){
-			System.out.print("Enter the rectangle/square's base: ");
-			base = scanner.nextDouble();
-			System.out.print("Enter the rectangle/square's height:");
-			height = scanner.nextDouble();
-			area = base * height;
-			System.out.println("The area of your rectangle/square is: " + area);
-		}
-		else if (objectToCalculate == 2){
-			System.out.print("Enter the triangle's base: ");
-			base = scanner.nextDouble();
-			System.out.print("Enter the triangle's height: ");
-			height = scanner.nextDouble();
-			area = base * height / 2;
-			System.out.println("The area of your triangle is: " + area);
-		}
-		else if (objectToCalculate == 3){
-			System.out.print("Enter the circle's radius: ");
-			radius = scanner.nextDouble();
 
-			area = Math.PI * Math.pow(radius, 2);
+		//Look the program unless user input ends the program. 
+		while (startAgain == true){
+			System.out.print("\nEnter the number of the object you wish to calculate: ");//print prints without a new line
+			objectToCalculate = scanner.nextInt();
+			//Calculations for area of a Rectangle/Square.
+			if (objectToCalculate == 1){
+				System.out.print("Enter the rectangle/square's base: ");
+				base = scanner.nextDouble();
+				System.out.print("Enter the rectangle/square's height:");
+				height = scanner.nextDouble();
+				area = base * height;
+				System.out.println("The area of your rectangle/square is: " + area);
+			}
+			//Calculations for area of a Triangle.
+			else if (objectToCalculate == 2){
+				System.out.print("Enter the triangle's base: ");
+				base = scanner.nextDouble();
+				System.out.print("Enter the triangle's height: ");
+				height = scanner.nextDouble();
+				area = base * height / 2;
+				System.out.println("The area of your triangle is: " + area);
+			}
+			//Calculations for area of a Circle.
+			else if (objectToCalculate == 3){
+				System.out.print("Enter the circle's radius: ");
+				radius = scanner.nextDouble();
+
+				area = Math.PI * Math.pow(radius, 2);
+				
+				System.out.println("The area of your circle is: " + area);
+			}
+			//Calculations for area of a Cube.
+			else if (objectToCalculate == 4){
+				System.out.print("Enter the cube's edge: ");
+				edge = scanner.nextDouble();
+				area = 6 * Math.pow(edge, 2);
+				System.out.println("The area of your cube is: " + area);
+			}
+			//Calculations for the area of a Cone
+			else if (objectToCalculate == 5){
+				System.out.print("Enter the cone's radius: ");
+				radius = scanner.nextDouble();
+				System.out.print("Enter the cone's height: ");
+				height = scanner.nextDouble();
+				area = Math.PI * radius * (radius + Math.sqrt(Math.pow(height, 2)+ Math.pow(radius, 2)));
+				System.out.println("The area of your cone is: " + area);
+			}
+			//Calculations for the area of a Sphere
+			else if (objectToCalculate == 6){
+				System.out.print("Enter the sphere's radius: ");
+				radius = scanner.nextDouble();
+				area = 4 * Math.PI * Math.pow(radius, 2);
+				System.out.println("The area of your sphere is: " + area);
+			}
+			//If number does not coorispond with one of the calculations ask if user would like to try again.
+			else{
+				System.out.println("Please enter a number that is on the list.");
+				System.out.println("Would you like to try again?");
+			}
+			//After calculation ask if the user would like to enter another calculation.
+			System.out.print("Would you like to calculate the area of another object? (y/n): ");
+			String userContinueInput = scanner.nextLine();
+			//If the user input is y continue to run the program, else quit.
 			
-			System.out.println("The area of your circle is: " + area);
-		}
-		else if (objectToCalculate == 4){
-			System.out.print("Enter the cube's edge: ");
-			edge = scanner.nextDouble();
-			area = 6 * Math.pow(edge, 2);
-			System.out.println("The area of your cube is: " + area);
-		}
-		//Calculations for the area of a Cone
-		else if (objectToCalculate == 5){
-			System.out.print("Enter the cone's radius: ");
-			radius = scanner.nextDouble();
-			System.out.print("Enter the cone's height: ");
-			height = scanner.nextDouble();
-			area = Math.PI * radius * (radius + Math.sqrt(Math.pow(height, 2)+ Math.pow(radius, 2)));
-			System.out.println("The area of your cone is: " + area);
-		}
-		//Calculations for the area of a Sphere
-		else if (objectToCalculate == 6){
-			System.out.print("Enter the sphere's radius: ");
-			radius = scanner.nextDouble();
-			area = 4 * Math.PI * Math.pow(radius, 2);
-			System.out.println("The area of your sphere is: " + area);
-		}
-		else{
-			System.out.println("Please enter a number that is on the list.");
-			System.out.println("Would you like to try again?");
-
+			if (userContinueInput == "y"){
+				startAgain = true;
+			}
+			else{
+				startAgain = false;
+			}
+			
 		}
 
 	}
